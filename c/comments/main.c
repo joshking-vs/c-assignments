4ea069cc19b4d483f19383979e2aad91b169cd4a
Joshua Agutu <128697219+joshking-vs@users.noreply.github.com>
2023-10-16 12:10:56 +0300

    Add files via upload

---
+++ c/comments/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //this is a comment
+    /*this is a multi line comment
+    the lines of code below will print hello world*/
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ c/variables/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  // Student data
+  int studentID = 13;
+  int studentAge = 20;
+  float studentFee = 90.70;
+  char studentGrade = 'A';
+
+  // Print variables
+  printf("Student id: %d\n", studentID);
+  printf("Student age: %d\n", studentAge);
+  printf("Student fee: %f\n", studentFee);
+  printf("Student grade: %c", studentGrade);
+
+  return 0;
+}
